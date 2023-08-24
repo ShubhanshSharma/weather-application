cfa4a299ff2164a02f437200a9484f241ce21b7f
shubhansh <shubhanshsharma030604@gmail.com>
2023-08-24 09:50:32 +0530

    starting project now

--- index.js
+++ index.js
@@ -1,43 +1,42 @@
-console.log("heliuu")
+// const API_key = "8214cae20a86d1c6e5ab0d1d45570f9d";
+// let btn = document.querySelector(".btn");
 
-const API_key = "8214cae20a86d1c6e5ab0d1d45570f9d";
-// let city_name = ;
-
-let btn = document.querySelector(".btn");
-
-
-async function fetchWeather() {
-    let city_name = document.querySelector(".city").value;
+// async function fetchWeather() {
+//     let city_name = document.querySelector(".city").value;
 
 
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_key}`);
+//     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_key}`);
 
-    const data = await response.json();
+//     const data = await response.json();
 
-    console.log("weather-->" , data);
-    dispWeather(data);
-}
+//     console.log("weather-->" , data);
+//     dispWeather(data);
+// }
 
-async function dispWeather(data){
-    let para = document.createElement("p");
-    let temp = `${(data?.main?.temp/10).toFixed(2)}`;
-    //console.log(temp);
-    para.textContent = `Temprature--> ${temp} C`;
+// async function dispWeather(data){
+//     let para = document.createElement("p");
+//     let temp = `${(data?.main?.temp/10).toFixed(2)}`;
+//     //console.log(temp);
+//     para.textContent = `Temprature--> ${temp} C`;
 
-    document.body.appendChild(para);
+//     document.body.appendChild(para);
 
-}
+// }
+//  // search button
+// btn.addEventListener('click', (e) => {
+//     // console.log(`${city_name}`);
 
-
-
-
-btn.addEventListener('click', (e) => {
-    // console.log(`${city_name}`);
-
-    try {
-        fetchWeather();
-    }
+//     try {
+//         fetchWeather();
+//     }
     
-    catch(err){
-        console.log("error" , err);
-    }})
+//     catch(err){
+//         console.log("error" , err);
+//     }})
+
+const userTab = document.querySelector("[user-tab]");
+const searchTab = document.querySelector("[search-tab]");
+const usercontainer = document.querySelector(".infocontainer");
+const searchForm = document.querySelector("[data-search]");
+const loadingScreen = document.querySelector(".loading")
+const userInfo = document.querySelector("[display-output]");
